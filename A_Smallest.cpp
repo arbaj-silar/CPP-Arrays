7e6ba1f6451470d5462918098daf7bc4aaaab561
Arbaj <silararbaj@gmail.com>
2026-04-13 05:55:29 +0530

    Finding smallest Value in Array

---
+++ A_Smallest.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int getSmallValue(int arr[], int n) {
+    int smallest = INT32_MAX;
+
+    for(int i=0; i<n; i++) {
+        if(smallest > arr[i]) {
+            smallest = arr[i];
+        }
+    }
+    return smallest;
+}
+
+int main() {
+    int arr[] = {5, 15, 22, 1, -15, -24};
+    int n = sizeof(arr)/sizeof(int);
+    cout<<getSmallValue(arr, n)<<endl;  
+    return 0;  
+}
