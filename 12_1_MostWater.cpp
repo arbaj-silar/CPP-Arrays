01df5c73c8574e38e6c4f517924216e9076f3913
Arbaj <silararbaj@gmail.com>
2026-04-14 06:47:54 +0530

    2 Pointer Approach

---
+++ 12_1_MostWater.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// Brute Approach
+int maxArea(vector<int>& height) {
+    int maxWater = 0;
+    int st = 0, end = height.size()-1;
+
+    while(st < end) {
+        int ht = min(height[st], height[end]);
+        int wd = end - st;
+        int currWater = wd*ht;
+        maxWater = max(maxWater, currWater);
+
+        if(height[st]>height[end]) {
+            end--;
+        } else {
+            st++;
+        }
+    }
+
+    return maxWater;
+}
+
+int main() {
+    vector<int> height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+
+    cout<<"Max area: "<<maxArea(height)<<endl;
+    return 0;
+}
